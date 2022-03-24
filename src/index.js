ddc1ff33c5ca56df4e39a33aaca01a8bc8398079
Kseniya <mylittleuniverse@list.ru>
2022-03-25 01:44:59 +0700

    add post functional

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import { rerenderEntireTree } from "./render";
 import state from "./redux/state";
-import { addPost } from "./redux/state";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App state={state} addPost={addPost} />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+rerenderEntireTree(state);
